ae87ef9481cf61ece307c63aad62413f9017902c
Kenan Rhoton <krhoton@redsauce.net>
2024-06-04 10:17:30 +0200

    negative numbers to 8 done

--- src/test/java/org/example/ConverterTest.java
+++ src/test/java/org/example/ConverterTest.java
@@ -8,28 +8,40 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ConverterTest {
 
     @Test
-    void convert1ToI() {
+    void convert1ToI() throws Exception {
         assertEquals("I", arabicToRoman(1));
     }
 
     @Test
-    void convert3ToIII() {
+    void convert3ToIII() throws Exception  {
         assertEquals("III", arabicToRoman(3));
     }
 
     @Test
-    void convert4ToIV() {
+    void convert4ToIV() throws Exception  {
         assertEquals("IV", arabicToRoman(4));
     }
 
     @Test
-    void convert5ToV() {
+    void convert5ToV() throws Exception  {
         assertEquals("V", arabicToRoman(5));
     }
 
     @Test
-    void convert6ToVI() {
+    void convert6ToVI() throws Exception  {
         assertEquals("VI", arabicToRoman(6));
+    }
+
+    @Test
+    void errorConverting0(){
+        Exception ex =  assertThrows(Exception.class, ()-> arabicToRoman(0));
+        assertEquals("El número debe ser mayor que 0", ex.getMessage());
+    }
+
+    @Test
+    void errorConvertingNegativeNumber(){
+        Exception ex =  assertThrows(Exception.class, ()-> arabicToRoman(-3));
+        assertEquals("El número debe ser mayor que 0", ex.getMessage());
     }
 
 }
