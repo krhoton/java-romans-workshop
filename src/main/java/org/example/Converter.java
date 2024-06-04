4a18483cbe6daf631c8ce727565f73a2ed418e57
Kenan Rhoton <krhoton@redsauce.net>
2024-06-04 10:31:58 +0200

    negative implementation

--- src/main/java/org/example/Converter.java
+++ src/main/java/org/example/Converter.java
@@ -7,12 +7,21 @@ public class Converter {
         if(num<=0){
             throw new Exception("El número debe ser mayor que 0");
         }
+        if(num >= 10 && num <= 19){
+            result.append("X");
+            num -= 10;
+        }
+        if(num == 9) {
+            result.append("IX");
+            num -= 9;
+        }
         if(num >= 5 && num <= 8) {
             result.append("V");
             num -= 5;
         }
         else if(num == 4){
-            return "IV";
+            result.append("IV");
+            num -= 4;
         }
 
         for (int i = 1; i <= num; i++) {
--- src/test/java/org/example/ConverterTest.java
+++ src/test/java/org/example/ConverterTest.java
@@ -33,6 +33,26 @@ public class ConverterTest {
     }
 
     @Test
+    void convert9ToIX() throws Exception {
+        assertEquals("IX", arabicToRoman(9));
+    }
+
+    @Test
+    void convert10ToIX() throws Exception {
+        assertEquals("X", arabicToRoman(10));
+    }
+
+    @Test
+    void convert13ToXIII() throws Exception {
+        assertEquals("XIII", arabicToRoman(13));
+    }
+
+    @Test
+    void convert19ToXIX() throws Exception {
+        assertEquals("XIX", arabicToRoman(19));
+    }
+
+    @Test
     void errorConverting0(){
         Exception ex =  assertThrows(Exception.class, ()-> arabicToRoman(0));
         assertEquals("El número debe ser mayor que 0", ex.getMessage());
